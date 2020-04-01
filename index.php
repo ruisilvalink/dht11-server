00fc9a0aa9e696ce23a63b26516c3dc879c9e061
Rui Silva <38074835+ruisilvalink@users.noreply.github.com>
2020-04-01 12:25:57 +0100

    Fixes col span

--- index.php
+++ index.php
@@ -5,19 +5,27 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="fontawesome-free-5.13.0-web/css/all.css" rel="stylesheet"> 
+	<style>
+		.fa-tint {
+			color: blue;
+		}
+		.fa-thermometer-half {
+			color: red;
+		}
+	</style>
 </head>
 <body class="container">
 	<br>
 	<br>
 	<div class="row">
-		<div class="col-sm text-center">
+		<div class="col-sm-12 text-center">
 			<h1 title="Humidity"><i class="fas fa-tint"></i></h1>
 			<h2><div id="humidityDiv"></div></h2>
 		</div>
 	</div>
 	<br/>
 	<div class="row">
-		<div class="col-sm text-center">
+		<div class="col-sm-12 text-center">
 			<h1 title="Temperature"><i class="fas fa-thermometer-half"></i></h1>
 			<h2><div id="temperatureDiv"></div></h2>
 		</div>
@@ -37,7 +45,7 @@ function update(data) {
 	if (data == null)
 		return;
 
-	$("#createdOnDiv")[0].innerHTML = data[0].created_on.date;
+	$("#createdOnDiv")[0].innerHTML = new Date(data[0].created_on.date).toLocaleString();
 	$("#humidityDiv")[0].innerHTML = data[0].humidity + " %";
 	$("#temperatureDiv")[0].innerHTML = data[0].temperature + " &#x2103";
 }
